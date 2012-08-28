97b509256ad5f8d7feba6f7249d01b960c60382e
Lewis Brown <lewix7@hotmail.com>
2012-08-28 15:33:48 +0100

    Refactor api into it's own class

---
+++ evernote_cli/evernoteapi.py
@@ -0,0 +1,40 @@
+import ConfigParser
+import logging
+from os.path import dirname
+
+from thrift.transport.THttpClient import THttpClient
+from thrift.protocol.TBinaryProtocol import TBinaryProtocol
+from evernote.edam.userstore import UserStore
+from evernote.edam.notestore import NoteStore
+
+class EvernoteApi(object):
+    def __init__(self):
+        config = ConfigParser.RawConfigParser()
+        config.read(dirname(__file__) + '/evernotecli.cfg')
+        self.developer_token = config.get('login_details', 'developer_token')
+
+
+    def _get_store_protocol(self, store_url):
+        user_store_client = THttpClient(store_url)
+        return TBinaryProtocol(user_store_client)
+
+    def _get_note_store_url(self):
+        user_store_url = 'http://sandbox.evernote.com/edam/user'
+        user_store_protocol = self._get_store_protocol(user_store_url)
+        user_store = UserStore.Client(user_store_protocol,
+                                      user_store_protocol)
+
+        note_store_url = user_store.getNoteStoreUrl(self.developer_token)
+        logging.debug('Retrieved NoteStore url: %s', note_store_url)
+
+        return note_store_url
+
+    def get_note_store(self):
+        note_store_url = self._get_note_store_url()
+        note_store_protocol = self._get_store_protocol(note_store_url)
+
+        note_store = NoteStore.Client(note_store_protocol,
+                                      note_store_protocol)
+        logging.debug('Retrived NoteStore: %s', note_store)
+
+        return note_store
--- integration_tests/api_tests.py
+++ integration_tests/api_tests.py
@@ -1,12 +1,15 @@
-import evernotecli
 from nose.tools import istest, assert_is_not_none
+
+from evernoteapi import EvernoteApi
 
 @istest
 def test_authentication_is_successful():
-    note_store_url = evernotecli._get_note_store_url()
+    api = EvernoteApi()
+    note_store_url = api._get_note_store_url()
     assert_is_not_none(note_store_url)
 
 @istest
 def test_note_store_is_retrieved_successfully():
-    note_store = evernotecli.get_note_store()
+    api = EvernoteApi()
+    note_store = api.get_note_store()
     assert_is_not_none(note_store)
