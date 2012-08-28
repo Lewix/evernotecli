635a59309f27a20f2a51c1f3fb7eaf713cde8c16
Lewis Brown <lewix7@hotmail.com>
2012-08-28 15:24:00 +0100

    Get note store. Need to patch thrift for the note store to work

--- evernote_cli/evernotecli.py
+++ evernote_cli/evernotecli.py
@@ -5,16 +5,20 @@ from os.path import dirname
 from thrift.transport.THttpClient import THttpClient
 from thrift.protocol.TBinaryProtocol import TBinaryProtocol
 from evernote.edam.userstore import UserStore
+from evernote.edam.notestore import NoteStore
 
 config = ConfigParser.RawConfigParser()
 print dirname(__file__)
 config.read(dirname(__file__) + '/evernotecli.cfg')
 developer_token = config.get('login_details', 'developer_token')
 
-def get_note_store_url():
+def _get_store_protocol(store_url):
+    user_store_client = THttpClient(store_url)
+    return TBinaryProtocol(user_store_client)
+
+def _get_note_store_url():
     user_store_url = 'http://sandbox.evernote.com/edam/user'
-    user_store_client = THttpClient(user_store_url)
-    user_store_protocol = TBinaryProtocol(user_store_client)
+    user_store_protocol = _get_store_protocol(user_store_url)
     user_store = UserStore.Client(user_store_protocol,
                                   user_store_protocol)
 
@@ -22,3 +26,13 @@ def get_note_store_url():
     logging.debug('Retrieved NoteStore url: %s', note_store_url)
 
     return note_store_url
+
+def get_note_store():
+    note_store_url = _get_note_store_url()
+    note_store_protocol = _get_store_protocol(note_store_url)
+
+    note_store = NoteStore.Client(note_store_protocol,
+                                  note_store_protocol)
+    logging.debug('Retrived NoteStore: %s', note_store)
+
+    return note_store
--- integration_tests/api_tests.py
+++ integration_tests/api_tests.py
@@ -3,5 +3,10 @@ from nose.tools import istest, assert_is_not_none
 
 @istest
 def test_authentication_is_successful():
-    note_store_url = evernotecli.get_note_store_url()
+    note_store_url = evernotecli._get_note_store_url()
     assert_is_not_none(note_store_url)
+
+@istest
+def test_note_store_is_retrieved_successfully():
+    note_store = evernotecli.get_note_store()
+    assert_is_not_none(note_store)
