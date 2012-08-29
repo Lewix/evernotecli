86be811bec7f6c159a23f86cb51bb71e2b02531c
Lewis Brown <lewix7@hotmail.com>
2012-08-29 19:48:01 +0200

    Run the integration tests properly

--- integration_tests/api_tests.py
+++ integration_tests/api_tests.py
@@ -1,26 +1,34 @@
 import random
 
 from nose.tools import istest, assert_is_not_none, assert_in
+from mock import patch
 
+import evernotecli
 from evernoteapi import EvernoteApi
 from notes import Note
 
 #TODO: is_not_none doesn't test much...
+#TODO: always use sandbox
+
+@istest
 def authentication_is_successful():
     api = EvernoteApi()
     note_store_url = api._get_note_store_url()
     assert_is_not_none(note_store_url)
 
+@istest
 def note_store_is_retrieved_successfully():
     api = EvernoteApi()
     note_store = api._get_note_store()
     assert_is_not_none(note_store)
 
+@istest
 def notebooks_are_listed_successfully():
     api = EvernoteApi()
     notebooks = api.list_notebooks()
     assert_is_not_none(notebooks)
 
+@istest
 def notes_are_listed_successfully():
     api = EvernoteApi()
     notes = api.list_notes("lewix's notebook")
