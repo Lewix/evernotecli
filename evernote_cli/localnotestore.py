4b2df15adb3963c70b293892f92fbf94a4d9d6ac
Lewis Brown <lewix7@hotmail.com>
2012-08-31 22:33:09 +0200

    Add caching decorator which takes multiple arguments

--- evernote_cli/localnotestore.py
+++ evernote_cli/localnotestore.py
@@ -1,36 +1,38 @@
-import logging
 from functools import wraps
 
+#TODO: store different operations for same function with different args
 class LocalNoteStore(object):
     def __init__(self):
         self.operations = {}
 
-    def _add_operation(self, changed_function, data_function):
+    def _add_operation(self, changed_function, data_function, *args, **kwargs):
         operation = {'changed' : changed_function(),
                      'changed_function' : changed_function,
-                     'data' : data_function(),
+                     'data' : data_function(*args, **kwargs),
                      'data_function' : data_function}
         self.operations[data_function] = operation
 
-    def get_if_changed(self, changed_function, data_function):
+    def get_if_changed(self, changed_function, data_function, *args, **kwargs):
         if data_function not in self.operations:
-            self._add_operation(changed_function, data_function)
+            self._add_operation(changed_function,
+                                data_function, *args, **kwargs)
             return self.operations[data_function]['data']
 
         changed = self.operations[data_function]['changed']
         new_changed = self.operations[data_function]['changed_function']()
         if changed != new_changed:
             self.operations[data_function]['changed'] = new_changed
-            return data_function()
+            return data_function(*args, **kwargs)
 
         return self.operations[data_function]['data']
 
 def cache(changed_function, local_note_store):
     def cache_decorator(data_function):
-        @wraps(function)
+        @wraps(data_function)
         def wrapper(*args, **kwargs):
             local_note_store.get_if_changed(changed_function,
-                                            data_function)
+                                            data_function,
+                                            *args, **kwargs)
 
         return wrapper
     return cache_decorator
--- unit_tests/store_tests.py
+++ unit_tests/store_tests.py
@@ -1,7 +1,7 @@
 from nose.tools import istest, assert_in, assert_not_in, assert_equal
 
 from changesstore import ChangesStore
-from localnotestore import LocalNoteStore
+from localnotestore import LocalNoteStore, cache
 from mock import Mock
 
 def create_excepting_note_store():
@@ -49,3 +49,23 @@ def local_store_only_updates_when_there_are_changes():
     note_store.get_if_changed(changed_function, data_function)
 
     assert_equal(data_function.call_count, 2)
+
+
+@istest
+def cache_decorator_caches_output_until_changes():
+    note_store = LocalNoteStore()
+    changed_function = Mock(return_value=1)
+    call_counter = Mock()
+
+    @cache(changed_function, note_store)
+    def data_function(arg):
+        call_counter()
+
+    data_function(1)
+    data_function(1)
+    assert_equal(call_counter.call_count, 1)
+
+    changed_function.return_value = 2
+    data_function(1)
+    data_function(1)
+    assert_equal(call_counter.call_count, 2)
